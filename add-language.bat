eaaf6336457f16a08a1db3d504696b29b45e8f8c
Nikola Yankov <nikola.yankov@gmail.com>
2022-10-28 23:03:55 +0300

    added ar and th language data  files, added bat file for language data files creation

---
+++ add-language.bat
@@ -0,0 +1,9 @@
+@echo off
+for %%x in (%*) do (
+    echo Adding %%~x language
+    Res.Translator.exe -r .\templates\data\index.en.json -d %%~x  --override
+    Res.Translator.exe -r .\templates\data\products\index_net.en.json -d %%~x  --overwrite
+    Res.Translator.exe -r .\templates\data\products\index_java.en.json -d %%~x  --overwrite
+    Res.Translator.exe -r .\templates\data\products\pair_net.en.json -d %%~x  --overwrite
+    Res.Translator.exe -r .\templates\data\products\pair_java.en.json -d %%~x  --overwrite
+)

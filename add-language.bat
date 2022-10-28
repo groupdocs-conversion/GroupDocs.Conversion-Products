0d1737382109e0ca5afff40fd4c23931377f6efd
Nikola Yankov <nikola.yankov@gmail.com>
2022-10-28 23:04:26 +0300

    fix in bat file for adding languages

--- add-language.bat
+++ add-language.bat
@@ -1,7 +1,7 @@
 @echo off
 for %%x in (%*) do (
     echo Adding %%~x language
-    Res.Translator.exe -r .\templates\data\index.en.json -d %%~x  --override
+    Res.Translator.exe -r .\templates\data\index.en.json -d %%~x  --overwrite
     Res.Translator.exe -r .\templates\data\products\index_net.en.json -d %%~x  --overwrite
     Res.Translator.exe -r .\templates\data\products\index_java.en.json -d %%~x  --overwrite
     Res.Translator.exe -r .\templates\data\products\pair_net.en.json -d %%~x  --overwrite
